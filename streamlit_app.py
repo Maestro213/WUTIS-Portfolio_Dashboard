2d22ee7488c2df9b4788e8178f1d1b2e5b8ad646
Kirill Gusev <34633491+Maestro213@users.noreply.github.com>
2024-03-02 18:17:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,37 +25,59 @@ COLOR_BULL = 'rgba(38,166,154,0.9)' # #26a69a
 COLOR_BEAR = 'rgba(239,83,80,0.9)'  # #ef5350
 
 
-df1 = pd.read_csv(r'data/ER.csv',index_col=0)
-df1 = df1.reset_index()
-df1['Date']= pd.to_datetime(df1['Date']).dt.date
-
+df_er = pd.read_csv(r'data/ER.csv',index_col=0)
+df_gm = pd.read_csv(r'data/GM.csv',index_col=0)
+df_algo = pd.read_csv(r'data/Algo.csv',index_col=0)
 
 
 
     
+def metrics(df, av_capital = 300000):
+    #this function calculate the metrics such as: Ann Ret, Ann Volatility, Sharpe ration, Maximum Drawdown, VaR
+    df['Date']= pd.to_datetime(df['Date']).dt.date
+    df['Return Pct'] = df["Adj Close"].pct_change()
+    df['Return Pct'][0] = 0
+    df['Return'] = df['Return Pct'] * av_capital
+    df['Cumulative Return Pct'] = (1 + df['Return Pct']).cumprod() 
+    df['Cumulative Return'] = (1 + df['Return Pct']).cumprod() * av_capital
+    
+    #Maximum Drawdown
+    # calculate the prior peaks for each day in our dataset
+    df["Prior Peak"] = df['Cumulative Return'].cummax()
+    # calculate the drawdown
+    df["Drawdown"] = (df['Cumulative Return'] - df["Prior Peak"]) / df["Prior Peak"]
+    max_drawdown = round(df["Drawdown"].min(),2)
+    n_periods = df.shape[0]
+    #Ann Return 
+    ret = round((1+df["Return Pct"]).prod()**(252/n_periods)-1,4) * 100 
+    #Ann Volatility
+    vol =  round(df["Return Pct"].std()*(252**0.5),2)
+    rf = 0.04**(1/2)
+    #Sharpe Ratio
+    sharpe_ratio = round((ret-0.04**(1/2))/vol,2)
+    
+    var = round( -np.percentile(df['Return'], (100 - 5)),2)
+    
+    met = pd.DataFrame({"Returns":ret, "Volatility":vol, "Sharpe Ratio": sharpe_ratio, "Maximum Drawdown":max_drawdown, "VaR":var},index=[0])
+    return df,met
 
-# Calculate the MAs for graphs
-df1['SMA-50'] = df1['Cumulative Return'].rolling(50).mean().dropna()
-df1['SMA-200'] = df1['Cumulative Return'].rolling(200).mean().dropna()
-# calculate the prior peaks for each day in our dataset
-df1["Prior Peak"] = df1['Cumulative Return'].cummax()
-# calculate the drawdown
-df1["Drawdown"] = (df1['Cumulative Return'] - df1["Prior Peak"]) / df1["Prior Peak"]
-#Calculating the Metrics 
-ret = round((1+df1.Return).prod()-1,4) * 100 
-vol =  round(df1.Return.std(),2)
-rf = 0.04**(1/2)
-sharpe_ratio = round((ret-0.04**(1/2))/vol,2)
-max_drawdown = round(df1["Drawdown"].min(),2)
-var = round( -np.percentile(df1['Cumulative Return'], (100 - 5)),2)
+sp = yf.download("SPY", start = "2023-09-30", end = "2024-03-01").reset_index()
+sp_met, sp = metrics(sp)
+er_met, df_er = metrics(df_er)
+gm_met, df_gm = metrics(df_gm)
+algo_met, df_algo = metrics(df_algo)
 
-met = pd.DataFrame({"Returns":ret, "Volatility":vol,"Sharpe Ratio": sharpe_ratio, "Maximum Drawdown":max_drawdown, "VaR":var},index=[0])
 
-df = pd.concat([df1.Date, df1['Cumulative Return'],df1['SMA-50']],axis = 1).dropna()
+df = pd.concat([df_er.Date, df_er['Cumulative Return'],df_gm['Cumulative Return'],df_algo['Cumulative Return'],sp['Cumulative Return']],axis = 1).dropna()
+df = df.set_axis(["Date","Equity Research", "Global Markets", "Algorithmic Trading",'S&P500 Benchmark'],axis=1)
+## Add to the whole portfolio
+df["WUTIS"] = df[["Equity Research", "Global Markets", "Algorithmic Trading"]].sum(axis=1)
+met = pd.concat([er_met, gm_met, algo_met,sp_met],axis = 0)
+met = met.set_axis(["Date","Equity Research", "Global Markets", "Algorithmic Trading",'S&P500 Benchmark'],axis=0)
 #######################
 # Dashboard Main Panel
 
-fig = px.line(df, x="Date", y=['Cumulative Return','SMA-50'], title='WUTIS Portfolio')
+fig = px.line(df, x="Date", y=["WUTIS", "Equity Research", "Global Markets", "Algorithmic Trading",'S&P500 Benchmark'], title='WUTIS Portfolio')
 fig.show()
 
 col = st.columns((8, 12), gap='medium')
@@ -70,7 +92,7 @@ with col[0]:
 #######################
 # Plot
 with col[1]:
-    st.line_chart(df, x="Date", y=['Cumulative Return','SMA-50'])
+    st.line_chart(df, x="Date", y=["WUTIS", "Equity Research", "Global Markets", "Algorithmic Trading",'S&P500 Benchmark'])
     
 
 
